4655ccd13c931c2365e78afc7e7508864cfaca61
Jan Monschke <jan.monschke@gmail.com>
2013-08-12 17:47:12 +0200

    show an animated icon after actions

--- js/controllers/gesture_controller.coffee
+++ js/controllers/gesture_controller.coffee
@@ -2,7 +2,7 @@ GestureController = ($scope, xbmcRemote) ->
   # set up the animations
   letter = angular.element('#letter')
   letter[0].addEventListener 'webkitAnimationEnd', ->
-    letter.removeClass 'grow'
+    letter.removeClass 'glow'
     console.log 'animationend'
 
   # register the gesture recognition
@@ -10,23 +10,30 @@ GestureController = ($scope, xbmcRemote) ->
   hammeredGestureTarget = gestureTarget.hammer()
 
   hammeredGestureTarget.on 'swipeup', -> 
-    letter.addClass 'grow'
+    letter.removeClass()
+    letter.addClass 'glow icon-up'
     xbmcRemote.up()
   hammeredGestureTarget.on 'swiperight', -> 
-    letter.addClass 'grow'
+    letter.removeClass()
+    letter.addClass 'glow icon-right'
     xbmcRemote.right()
   hammeredGestureTarget.on 'swipedown', -> 
-    letter.addClass 'grow'
+    letter.removeClass()
+    letter.addClass 'glow icon-down'
     xbmcRemote.down()
   hammeredGestureTarget.on 'swipeleft', -> 
-    letter.addClass 'grow'
+    letter.removeClass()
+    letter.addClass 'glow icon-left'
     xbmcRemote.left()
   gestureTarget.on 'click', -> 
-    letter.addClass 'grow'
+    letter.removeClass()
+    letter.addClass 'glow icon-up-hand'
     xbmcRemote.select()
   hammeredGestureTarget.on 'doubletap', -> 
-    letter.addClass 'grow'
+    letter.removeClass()
+    letter.addClass 'glow icon-ccw'
     xbmcRemote.back()
   hammeredGestureTarget.on 'hold', -> 
-    letter.addClass 'grow'
+    letter.removeClass()
+    letter.addClass 'glow icon-up-circled2'
     xbmcRemote.contextMenu()
--- js/controllers/gesture_controller.js
+++ js/controllers/gesture_controller.js
@@ -5,37 +5,44 @@ GestureController = function($scope, xbmcRemote) {
   var gestureTarget, hammeredGestureTarget, letter;
   letter = angular.element('#letter');
   letter[0].addEventListener('webkitAnimationEnd', function() {
-    letter.removeClass('grow');
+    letter.removeClass('glow');
     return console.log('animationend');
   });
   gestureTarget = angular.element('#gesture-target');
   hammeredGestureTarget = gestureTarget.hammer();
   hammeredGestureTarget.on('swipeup', function() {
-    letter.addClass('grow');
+    letter.removeClass();
+    letter.addClass('glow icon-up');
     return xbmcRemote.up();
   });
   hammeredGestureTarget.on('swiperight', function() {
-    letter.addClass('grow');
+    letter.removeClass();
+    letter.addClass('glow icon-right');
     return xbmcRemote.right();
   });
   hammeredGestureTarget.on('swipedown', function() {
-    letter.addClass('grow');
+    letter.removeClass();
+    letter.addClass('glow icon-down');
     return xbmcRemote.down();
   });
   hammeredGestureTarget.on('swipeleft', function() {
-    letter.addClass('grow');
+    letter.removeClass();
+    letter.addClass('glow icon-left');
     return xbmcRemote.left();
   });
   gestureTarget.on('click', function() {
-    letter.addClass('grow');
+    letter.removeClass();
+    letter.addClass('glow icon-up-hand');
     return xbmcRemote.select();
   });
   hammeredGestureTarget.on('doubletap', function() {
-    letter.addClass('grow');
+    letter.removeClass();
+    letter.addClass('glow icon-ccw');
     return xbmcRemote.back();
   });
   return hammeredGestureTarget.on('hold', function() {
-    letter.addClass('grow');
+    letter.removeClass();
+    letter.addClass('glow icon-up-circled2');
     return xbmcRemote.contextMenu();
   });
 };
